5bee0903c2f3ecd60d4de44d666021e84243b631
kimyoungyin <yin199859@gmail.com>
2023-11-30 17:42:22 +0900

    stateful dice 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,89 +13,54 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage(
-                    "images/profile.jpg",
-                  ),
-                ),
-                const Text(
-                  "Yeongyin Kim",
-                  style: TextStyle(
-                    fontFamily: "Pacifico",
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  "FRONTEND DEVELOPER",
-                  style: TextStyle(
-                    fontFamily: "SourceSans3",
-                    fontSize: 20,
-                    color: Colors.teal[100],
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.5,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                  width: 150,
-                  child: Divider(
-                    color: Colors.teal.shade100,
-                  ),
-                ),
-                Card(
-                    color: Colors.white,
-                    margin: const EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 25,
-                    ),
-                    child: ListTile(
-                      leading: const Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        "010 1234 1234",
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontSize: 20,
-                          fontFamily: "SourceSans3",
-                        ),
-                      ),
-                    )),
-                Card(
-                  color: Colors.white,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 25,
-                  ),
-                  child: ListTile(
-                    leading: const Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      "yin199859@gmail.com",
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontSize: 20,
-                        fontFamily: "SourceSans3",
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dicee'),
+          backgroundColor: Colors.transparent,
+        ),
+        body: const DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int firstDice = 1;
+  int secondDice = 1;
+
+  void handleClickDice() {
+    setState(() {
+      firstDice = Random().nextInt(6) + 1;
+      secondDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: handleClickDice,
+                child: Image.asset("images/dice$firstDice.png"),
+              ),
             ),
-          ),
+            Expanded(
+              child: TextButton(
+                onPressed: handleClickDice,
+                child: Image.asset("images/dice$secondDice.png"),
+              ),
+            ),
+          ],
         ),
       ),
     );
